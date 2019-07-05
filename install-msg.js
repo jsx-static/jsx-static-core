3eb3730ebb6537175ba8c29641349beabb35fb75
alexa <alexa.griffin42@gmail.com>
2019-07-05 00:40:59 -0400

    this will be useless soon

---
+++ install-msg.js
@@ -0,0 +1,7 @@
+console.log("**************************************************************")
+console.log("*                                                            *")
+console.log("*  !!  You need to run this command for things to work:  !!  *")
+console.log("*                                                            *")
+console.log("**************************************************************")
+
+console.log("npm i @babel/core @babel/plugin-transform-react-jsx @babel/preset-env babel-loader react")
