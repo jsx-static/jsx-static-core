90fa8ff414cc10322ad05a8a8e64878bdc45f5cf
alexa <alexa.griffin42@gmail.com>
2019-07-04 21:23:22 -0400

    jesus christ it works

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import * as path from "path"
 import * as fs from "fs"
+import * as path from "path"
+
 import recursive from "recursive-readdir"
+import webpack from "webpack"
+import MemFS from "memory-fs"
 
 import { error } from "./error"
+import { getPath } from "./util"
 
 interface BuildConfig {
   siteDir: string
@@ -13,14 +17,46 @@ const defaultConfig: BuildConfig = {
 }
 
 function build(config: BuildConfig) {
+  const mfs = new MemFS()
   return new Promise((res, rej) => {
-    if(!config && !fs.existsSync(path.join(path.resolve("."), "jsxs.config.json"))) config = defaultConfig
+    if(!config && !fs.existsSync(getPath("jsxs.config.json"))) config = defaultConfig
     
-    recursive(path.join(path.resolve("."), config.siteDir), (err, files) => {
-      if(err) return error(err)
+    recursive(getPath(config.siteDir), (err, files) => {
+      if(err) rej(error(err))
       for(let i = 0; i < files.length; i++) {
+        const packer = webpack({
+          entry: files[i],
+          output: {
+            filename: path.basename(files[i]),
+            path: "/out"
+          },
+          module: {
+            rules: [
+              {
+                test: /\.jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ["@babel/plugin-transform-react-jsx"]
+                  }
+                }
+              }
+            ]
+          },
+        })
+        packer.outputFileSystem = mfs
+
         console.log(files[i])
-      }
+        packer.run((err, stats) => {
+          //@ts-ignore
+          fs.writeFile(path.basename(files[i]), packer.outputFileSystem.data.out[path.basename(files[i])], (err) => {
+            console.log("hi")
+          })
+          if(stats.hasErrors()) rej(error(stats.toJson().errors))
+        })
+        }
     })
   })
 }
