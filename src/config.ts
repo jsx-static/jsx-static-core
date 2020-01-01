ef9be9681ea9059e20022b6d641d8aebf65e7207
alexa <alexa.griffin42@gmail.com>
2020-01-01 05:29:55 -0500

    make it better

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,46 @@
 import webpack from "webpack"
 import recursive from "recursive-readdir-ext"
 import path from "path"
+import fs from "fs"
+import MemoryFileSystem from "memory-fs"
 
-export function getConfig(memIn: boolean, memOut: boolean): webpack.Configuration {
-  return {
+export interface JsxsConfig {
+  inputFs?: any
+  outputFs?: any
+  siteDir?: string
+  outputDir?: string
+  componentDir?: string
+  dataDir?: string
+  dataEntry?: string,
+  root?: string
+}
+
+export const defaultConfig: JsxsConfig = {
+  inputFs: fs,
+  outputFs: fs,
+  siteDir: "/site",
+  outputDir: "/build",
+  componentDir: "/components",
+  dataDir: "/data",
+  dataEntry: "index.js",
+  root: path.resolve(".")
+}
+
+export function getJsxsConfig(config: JsxsConfig): JsxsConfig {
+  if(config) {
+    let out = { ...defaultConfig, ...config } 
+    if(out.outputFs instanceof(MemoryFileSystem) && !config.root) out.root = ""
+    return out
+  }
+  else if(fs.existsSync("jsxs.config.json")) return { 
+    ...JSON.parse(fs.readFileSync("jsxs.config.json", "utf8")), 
+    ...defaultConfig 
+  }
+  else return defaultConfig
+}
+
+export function genWebpack(config: JsxsConfig, memIn: boolean, memOut: boolean): webpack.Compiler {
+  const wpConfig: webpack.Configuration = {
     entry: () => 
     new Promise((res, rej) => {
       recursive(path.join(memIn ? "" : path.resolve("."), "site"), {}, (err, files) => {
@@ -48,4 +85,11 @@ export function getConfig(memIn: boolean, memOut: boolean): webpack.Configuratio
       new webpack.ProvidePlugin({ "React": 'react' })
     ]
   }
+
+  let compiler = webpack(wpConfig)
+
+  compiler.outputFileSystem = new MemoryFileSystem()
+  compiler.inputFileSystem = config.inputFs
+
+  return compiler
 }
--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,54 @@
-import webpack from "webpack"
-import { getConfig } from "./config"
-import MemoryFileSystem from "memory-fs"
+import { genWebpack, JsxsConfig, getJsxsConfig } from "./config"
 import { genHTML } from "./compiler"
-import express from "express"
-import sockjsNode from "sockjs"
-import http from "http"
 import path from "path"
+import webpack from "webpack"
 
-const sockInjection = `
-<script src="sockjs.min.js"></script>
-<script>
-var sockjs_url = '/echo'
-var sockjs = new SockJS(sockjs_url)
-
-sockjs.onopen = function() { console.log('openning socket', sockjs.protocol) }
-sockjs.onmessage = function(e) {
-  console.log(e)
-  if(JSON.parse(e.data).msg === "reload") location.reload()
-}
-sockjs.onclose = function() { console.log('closing socket') }
-</script>
-`
-
-export function dev() {
-  const packer = webpack(getConfig(false, true))
-  const webpackOutputFs = new MemoryFileSystem()
-  
-  packer.outputFileSystem = webpackOutputFs
-  
-  
-  packer.watch({}, (err, stats) => {
-    for(let file in webpackOutputFs.data) {
-      serverFs.writeFile("/" + file.replace(".jsx", ".html"), genHTML(webpackOutputFs.data[file].toString()) + sockInjection, console.log)
+function prepareWorkspace(config: JsxsConfig) {
+  function create(dir: string) {
+    if(!config.inputFs.existsSync(path.join(config.root, dir))) {
+      console.log(`${dir} not found, creating ${dir}`)
+      config.inputFs.mkdir(path.join(config.root, dir), err => { if(err) console.error(`failed to create ${dir}`) })
     }
+  }
 
-    connections.forEach(conn => conn.emit("data", { msg: "reload" }))
-  })
+  create(config.outputDir)
+  create(config.componentDir)
+  create(config.dataDir)
+  create(config.siteDir)
+}
+
+const buildCallback = (err: any, stats: webpack.Stats, config: JsxsConfig) => {
+  if(err) console.error(err)
+  else {
+    //@ts-ignore // data isn't normally a part of a fs but in this case it is because it will always be memfs
+    for(let file in stats.compilation.compiler.outputFileSystem.data) {
+      config.outputFs.writeFile(
+        path.join(config.root, config.outputDir, file.replace(".jsx", ".html")), 
+        //@ts-ignore // ^
+        genHTML(stats.compilation.compiler.outputFileSystem.data[file].toString()), 
+        (err) => { if(err) console.error(err) })
+    }
+  }
+}
+
+export function watch(config?: JsxsConfig) {
+  config = getJsxsConfig(config)
+  const packer = genWebpack(config, false, true)
+
+  prepareWorkspace(config)
   
+  packer.watch({}, (err, stats) => buildCallback(err, stats, config))
+}
 
-  // dev server stuff
+export function build(config?: JsxsConfig) {
+  config = getJsxsConfig(config)
+  const packer = genWebpack(config, false, true)
 
-  const serverFs = new MemoryFileSystem()
-  
-  const echo = sockjsNode.createServer({ 
-    prefix: "/echo",
-  })
-
-  const server = express()
-  server.use(express.static(path.join(path.dirname(require.resolve("sockjs-client")), "..", "dist")))
-  
-  server.get("/*", (req, res) => {
-    res.setHeader("Content-Type", "text/html")
-    res.send(serverFs.readFileSync(req.path))
-  })
-
-  
-  const httpServer = http.createServer(server)
-  
-  let connections = []
-
-  echo.on('connection', conn => {
-    connections.push(conn)
-    conn.on('data', msg => {
-      conn.write(JSON.stringify(msg))
-    })
-  })
-
-  echo.installHandlers(httpServer, {
-    prefix: "/echo"
-  })
-
-  httpServer.listen(8000, () => {})
+  prepareWorkspace(config)
+  packer.run((err, stats) => buildCallback(err, stats, config))
 }
 
 export default {
-  dev
+  build,
+  watch
 }
